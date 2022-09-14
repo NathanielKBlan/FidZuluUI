9d1f22d2eb345258aaae64cd3b690e298e374c74
Patrick McDonough <patmcdee98@gmail.com>
2022-09-14 11:55:59 -0400

    added items and models

---
+++ src/app/items/items.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+
+import { Item } from '../models/item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsService {
+
+  static readonly baseURL = 'http://localhost:';
+
+  private getItemsWithFilter(filterParam: string): Observable<Item[]> {
+    return this.http.get<Item[]>(ItemService.url, {
+        params: {
+            filter: filterParam
+        }
+    })
+        .pipe(catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it.
+        console.error('An error occurred:', error.error.message);
+    } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues 
+        console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Unable to contact service; please try again later.');
+  };
+
+
+  constructor(private http: HttpClient) { }
+}
